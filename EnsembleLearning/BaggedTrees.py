73d342dc4da19481937b1d7b744f969f28b08774
pink general <80594038+yuzew@users.noreply.github.com>
2022-10-20 13:45:32 -0600

    Add files via upload

---
+++ EnsembleLearning/BaggedTrees.py
@@ -0,0 +1,62 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct 20 11:47:37 2022
+
+@author: Yuze
+"""
+
+from math import inf
+import sys
+sys.path.append("../DecisionTree")
+
+import DecisionTree
+
+
+#####
+# Purpose: Perform the bagged decision tree algorithm
+#####
+def BaggedDecisionTrees(data, attrDict, labelCol, gainMethod, T, samplesize):
+    import random, math
+    trees = []
+
+    for i in range(T):
+        # draw m samples uniformly random with replacement from data
+        samples = random.choices(data, k=math.ceil(samplesize*len(data)))
+
+        # learn a decision tree based on those samples with no depth limit
+        root = DecisionTree.Tree(None)
+        root.depth = 0
+        DecisionTree.ID3(samples, attrDict, labelCol, root, inf, gainMethod, None)
+        trees.append(root)
+
+    # return the list of trees
+    return trees
+
+#####
+# Purpose: Using the list of trees from the BaggedDecisionTrees and predict
+#          the label of the data. Uses a plurality vote to predict
+#####
+def predict(data, predictCol, trees):
+    import copy
+    predictData = copy.deepcopy(data)
+
+    for example in predictData:
+        # pick the label predicted the most
+        example[predictCol] = predict_example(example, predictCol, trees)
+
+    return predictData
+
+#####
+# Purpose: Predict a single example using plurality vote
+#####
+def predict_example(example, predictCol, trees):
+    labelVotes = {}
+    # predict the example label using all the trees
+    for root in trees:
+        thisPredict = DecisionTree.predict_example(example, predictCol, root)
+        if thisPredict not in labelVotes:
+            labelVotes[thisPredict] = 1
+        else:
+            labelVotes[thisPredict] += 1
+    # pick the label predicted the most
+    return max(labelVotes, key=labelVotes.get)
---
+++ EnsembleLearning/RandomForest.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct 20 11:27:39 2022
+
+@author: Yuze
+"""
+
+from math import inf
+import sys
+sys.path.append("../DecisionTree")
+
+import DecisionTree
+
+# Purpose: Learn a Random Forests. Return a list of decision trees to use for prediction
+#####
+def RandomForests(data, attrDict, labelCol, gainMethod, T, featureSetSize, samplesize):
+    import random, math
+    forest = []
+
+    for i in range(T):
+        # draw m samples uniformly random with replacement from data
+        samples = random.choices(data, k=math.ceil(samplesize*len(data)))
+
+        # learn a full decision tree based on those samples with no depth limit
+        root = DecisionTree.Tree(None)
+        root.depth = 0
+        DecisionTree.ID3_RandTree(samples, attrDict, labelCol, root, inf, gainMethod, None, featureSetSize)
+        forest.append(root)
+
+    # return the list of trees
+    return forest
+
+#####
+
+# Purpose: Using the list of trees from RandomForests, predict
+#          the label of the data using a plurality vote
+
+def predict(data, predictCol, forest):
+    import copy
+    predictData = copy.deepcopy(data)
+
+    for example in predictData:
+        # pick the label predicted the most
+        example[predictCol] = predict_example(example, predictCol, forest)
+
+    return predictData
+
+# Purpose: Predict a single example using plurality vote
+def predict_example(example, predictCol, forest):
+    labelVotes = {}
+    # predict the example label using all the trees
+    for root in forest:
+        thisPredict = DecisionTree.predict_example(example, predictCol, root)
+        if thisPredict not in labelVotes:
+            labelVotes[thisPredict] = 1
+        else:
+            labelVotes[thisPredict] += 1
+    # pick the label predicted the most
+    return max(labelVotes, key=labelVotes.get)
